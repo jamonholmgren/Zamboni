f20764f8ee9aefc214869a7333fceb8ebb84c9e9
Jamon Holmgren <jamonholmgren@gmail.com>
2022-02-03 13:12:17 -0800

    Initial Commit

---
+++ Zamboni/ZamboniApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZamboniApp.swift
+//  Zamboni
+//
+//  Created by Jamon Holmgren on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ZamboniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
